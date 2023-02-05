d43c1923f87003110ddd44d643004240c772f6eb
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-02-05 02:26:18 -0300

    feat: add taskboard with no tasks

--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -1,17 +1,27 @@
 import styles from './TaskBoard.module.css'
 
+import clipboard from '../assets/clipboard.svg'
+
 export function TaskBoard() {
   return (
-    <div className={styles.header}>
-      <div className={styles.createdTasks}>
-        <span>Tarefas criadas</span>
-        <button>0</button>
+    <>
+      <div className={styles.header}>
+        <div className={styles.createdTasks}>
+          <span>Tarefas criadas</span>
+          <button>0</button>
+        </div>
+
+        <div className={styles.completedTasks}>
+          <span>Concluídas</span>
+          <button>2 de 5</button>
+        </div>
       </div>
 
-      <div className={styles.completedTasks}>
-        <span>Concluídas</span>
-        <button>2 de 5</button>
+      <div className={styles.board}>
+        <img src={clipboard} alt="" />
+        <span>Você ainda não tem tarefas cadastradas</span>
+        <span>Crie tarefas e organize seus itens a fazer</span>
       </div>
-    </div>
+    </>
   )
 }
