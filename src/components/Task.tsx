7d093bb1de93c76ca78ee1280f370a392f3ac6b7
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-02-06 19:16:32 -0300

    feat: add strikethrough text on task check

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,17 +1,35 @@
 import styles from './Task.module.css'
 
 import { Trash } from 'phosphor-react'
+import { useState } from 'react'
 
 export function Task() {
+  const [isChecked, setIsChecked] = useState(false)
+
+  function handleTaskCheck() {
+    setIsChecked(!isChecked)
+  }
+
   return (
     <div className={styles.task}>
       <div className={styles.checkboxContainer}>
-        <input type="checkbox" className={styles.checkbox} />
+        <input
+          type="checkbox"
+          onChange={handleTaskCheck}
+          className={styles.checkbox}
+        />
       </div>
-      <span>
-        Integer urna interdum massa libero auctor neque turpis turpis semper.
-        Duis vel sed fames integer.
-      </span>
+      {isChecked ? (
+        <s>
+          Integer urna interdum massa libero auctor neque turpis turpis semper.
+          Duis vel sed fames integer.
+        </s>
+      ) : (
+        <span>
+          Integer urna interdum massa libero auctor neque turpis turpis semper.
+          Duis vel sed fames integer.
+        </span>
+      )}
 
       <button title="Deletar task">
         <Trash size={24} />
