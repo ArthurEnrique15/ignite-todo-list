1290f7c591960821571485e734537f57a3c61fde
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-02-07 10:45:16 -0300

    feat: prevent the creation of tasks with text empty

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,7 +1,7 @@
 import styles from './AddTask.module.css'
 
 import plus from '../assets/plus.svg'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react'
 
 type AddTaskProps = {
   onAddTask: (description: string) => void
@@ -10,29 +10,43 @@ type AddTaskProps = {
 export function AddTask({ onAddTask }: AddTaskProps) {
   const [newTaskText, setNewTaskText] = useState('')
 
-  function handleAddTask() {
+  function handleAddTask(event: FormEvent) {
+    // * impede que a página seja atualizada no submit do form
+    event.preventDefault()
+
     const description = newTaskText
     setNewTaskText('')
     onAddTask(description)
   }
 
   function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    // * Limpa a mensagem de erro
+    event.target.setCustomValidity('')
+
     const newTaskText = event.target.value
     setNewTaskText(newTaskText)
   }
 
+  function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('Esse campo é obrigatório!')
+  }
+
+  const newTaskIsEmpty = newTaskText === ''
+
   return (
-    <div className={styles.addTask}>
+    <form onSubmit={handleAddTask} className={styles.addTask}>
       <input
         type="text"
         value={newTaskText}
         onChange={handleNewTaskChange}
+        onInvalid={handleNewTaskInvalid}
         placeholder="Adiciona uma nova tarefa"
+        required
       ></input>
-      <button onClick={handleAddTask}>
+      <button type="submit" disabled={newTaskIsEmpty}>
         Criar
         <img src={plus} alt="" />
       </button>
-    </div>
+    </form>
   )
 }
